e22c7b8501d2023c295fe6d272cc59053dac8559
David_one8 <davidfule18@gmail.com>
2026-02-12 18:54:57 +0530

    feat: implement sidebar state management in DashboardLayout for improved interactivity

--- client/src/components/layout/DashboardLayout.jsx
+++ client/src/components/layout/DashboardLayout.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 
 const DashboardLayout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <div className="flex h-screen overflow-hidden bg-gray-50 dark:bg-dark-950">
-      <Sidebar />
+      <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
       <div className="flex-1 flex flex-col overflow-hidden">
-        <Navbar />
-        <main className="flex-1 overflow-y-auto p-6">
+        <Navbar onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
+        <main className="flex-1 overflow-y-auto p-4 lg:p-6">
           <div className="max-w-7xl mx-auto">
             <Outlet />
           </div>
